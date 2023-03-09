8f3900110d588533e340b89cc6d4fdd12601ffe7
AndyJCR8 <andy_cr@umes.edu.gt>
2023-03-08 23:31:11 -0600

    Estructura básica del navbar y comentarios sobre cómo implementar los estilos

---
+++ Frontend/minizon/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import './Navbar.scss'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+      <div className='navbarOptions'>
+        <div className='navbarItems'>
+
+        </div>
+      </div>
+      <div className='navbarTitle'>
+
+      </div>
+      <div className='navbarActions'>
+        <div className='navbarSearch'>
+
+        </div>
+        <div className='navbarItems'>
+
+          
+          <div className='navbarItem'>
+            {/* SI ES MUY PEQUEÑA LA PANTALLA ACTIVAR EL SIDEBAR POR MEDIO DE UN BOTÓN*/}
+          </div>
+        </div>
+      </div>
+    </nav>
+  )
+}
