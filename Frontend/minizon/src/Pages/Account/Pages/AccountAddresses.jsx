127848635c82f562254288f74b3b7688ccc42677
AndyJCR8 <andy_cr@umes.edu.gt>
2023-05-10 23:41:56 -0600

    Cambios en las direcciones

--- Frontend/minizon/src/Pages/Account/Pages/AccountAddresses.jsx
+++ Frontend/minizon/src/Pages/Account/Pages/AccountAddresses.jsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import { Link, Navigate, Route, Routes, useNavigate, useParams } from 'react-router-dom';
 import './AccountPages.scss'
 import React, { useState, useEffect, useCallback, ChangeEvent } from 'react'
 import useFormAnswer from '../../../Hooks/useFormAnswer';
@@ -11,10 +11,21 @@ export default function AccountAddresses({UserID}) {
   const [title, setTitle] = useState("Mis direcciones");
   const [addresses, setAddresses] = useState([]);
 
+  const [idAddress, setIdAddress] = useState(0);
+
   const navigate = useNavigate()
 
   const handleNavigate = useCallback(
     () => { if(returnButton) navigate(-1) }
+  )
+  const handleAddresses = useCallback(
+    async () => {
+      const res = await axios.get(`${import.meta.env.VITE_SERVICE_1}/direcciones`, {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
+      
+      setAddresses(res.data.direcciones)
+    },
   )
 
   useEffect(() => {
@@ -24,7 +35,10 @@ export default function AccountAddresses({UserID}) {
   useEffect(() => {
     if(!location.pathname.includes("@")) setReturnButton(false)
     
-    if(location.pathname == "/account/addresses") setTitle("Mis direcciones")
+    if(location.pathname == "/account/addresses") {
+      setTitle("Mis direcciones")
+      handleAddresses()
+    }
   }, [navigate]);
   
   return (
@@ -39,14 +53,23 @@ export default function AccountAddresses({UserID}) {
             <>
               {
                 addresses.map(address => {
+                  
                   return (
                     <section className='address'>
-                      
+                      <p className='addressInfo'>{address.Direccion}</p>
+                      <div className='place'>
+                        <p>{address.municipio.Nombre}</p>
+                        <p>{address.municipio.departamento.Nombre}</p>
+                      </div>
+                      <div className='options'>
+                        <Link to={`@editAddress/${address.IDDireccion}`}>Editar</Link>
+                        <Link to="">Eliminar</Link>
+                      </div>
                     </section>
                   )
                 })
               }
-              <section className='address addressNew'>
+              <section className='addressNew'>
                 <Link to='@addNewAddress' onClick={() => setTitle("Nueva dirección")}>
                   <i className='fa-solid fa-plus'></i>
                   <p>Agregar nueva dirección</p>
@@ -55,6 +78,8 @@ export default function AccountAddresses({UserID}) {
             </>
           } path='/'/>
           <Route element={<NewAddress/>} path='@addNewAddress'/>
+          <Route element={<EditAddress/>} path='@editAddress/:id'/>
+          
         </Routes>
       </main>
     </div>
@@ -179,6 +204,142 @@ function NewAddress() {
           <div className={`formAnswer ${answerType}`}>{answer}</div>
         </div>
         <button type='submit' className='button primary'>Agregar dirección</button>
+      </footer>
+    </form>
+  )
+}
+
+function EditAddress() {
+
+  const [departamentos, setDepartamentos] = useState([]);
+  const [municipios, setMunicipios] = useState([]);
+  const [address, setAddress] = useState({});
+  
+  const { id } = useParams()
+
+  const [answer, answerType, answerActive, setAnswerFormData] = useFormAnswer()
+  
+  /**
+   * @param {SubmitEvent} e
+   */
+  const editAddress = (e) => {
+    e.preventDefault()
+    const form = e.currentTarget
+
+    const formResponse = (answer, typeAnswer) => {
+      setAnswerFormData(answer, typeAnswer, true)
+      setTimeout(() => {
+        setAnswerFormData(answer, typeAnswer, false)
+      }, 2000);
+    }
+
+    if(form['IDMunicipio'].value == -1) formResponse("Debe seleccionar un municipio antes de continuar", "error");
+    
+    (async () => {
+      const res = await axios.put(`${import.meta.env.VITE_SERVICE_1}/direccion`, { Direccion: form['Direccion'].value, IDMunicipio: form['IDMunicipio'].value }, {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
+      if(res.status == 200) formResponse("Dirección editada correctamente", "success")
+      else formResponse("Ha ocurrido un error al editar la dirección", "error")
+    })();
+  }
+
+  /**
+   * @param {ChangeEvent} e 
+   */
+  const departamentoChanged = (e) => {
+    (async () => {
+      const idDepartamento = e.currentTarget.value
+
+      const resMunicipios = await axios.get(`${import.meta.env.VITE_SERVICE_1}/municipios?idDepartamento=${idDepartamento}`, {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
+      
+      if(!Object.keys(resMunicipios.data).includes("message"))
+        setMunicipios(resMunicipios.data.municipios)
+      else setMunicipios([])
+    })()
+  }
+
+  useEffect(() => {
+    
+    (async () => {
+      const resDepartamentos = await axios.get(`${import.meta.env.VITE_SERVICE_1}/departamentos`, {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
+      setDepartamentos(resDepartamentos.data.departamentos)
+      
+      const lastDepartamento = resDepartamentos.data.departamentos.at(0)
+      const resMunicipios = await axios.get(`${import.meta.env.VITE_SERVICE_1}/municipios?idDepartamento=${lastDepartamento.IDDepartamento}`, {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
+      
+      
+      if(!Object.keys(resMunicipios.data).includes("message"))
+        setMunicipios(resMunicipios.data.municipios)
+      
+      const resAddress = await axios.get(`${import.meta.env.VITE_SERVICE_1}/direccion?idDireccion=${id}`, {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
+      setAddress(resAddress.data.direccion)
+    })()
+  }, []);
+  
+  return (
+    <form className='addressesForm' onSubmit={editAddress}>
+      <header>
+        <h2>Edición de la dirección: </h2>
+        {
+          Object.keys(address ?? {}).length > 0 ?
+          <h3 className='editAddressText'>{address.Direccion} - {address.municipio.Nombre} {address.municipio.departamento.Nombre}</h3> : null
+        }
+      </header>
+      <main>
+        <div className='formItem'>
+          <label>Dirección</label>
+          <input defaultValue={Object.keys(address ?? {}).length > 0 ? address.Direccion : null} name='Direccion' className='formInput' placeholder='1ra. av 3ra calle zona 1' required/>
+        </div>
+        <div className='formItem'>
+          <label>Departamento</label>
+          <div className='sectionContainer'>
+            <select defaultValue={Object.keys(address ?? {}).length > 0 ? address.municipio.IDDepartamento : ""} name='IDDepartamento' className='formSelect' onChange={departamentoChanged} required>
+              {
+                departamentos.map((departamento, i) => {
+                  return (
+                    <option key={i} value={departamento.IDDepartamento}>
+                      {departamento.Nombre}
+                    </option>
+                  )
+                })
+              }
+            </select>
+            <i className='fa-solid fa-caret-down'></i>
+          </div>
+        </div>
+        <div className='formItem'>
+          <label>Municipio</label>
+          <div className='sectionContainer'>
+            <select defaultValue={Object.keys(address ?? {}).length > 0 ? address.IDMunicipio : ""} name='IDMunicipio' className='formSelect' required>
+              {
+                municipios.length > 0 ?
+                municipios.map((municipio, i) => {
+                  return (
+                    <option key={i} value={municipio.IDMunicipio}>
+                      {municipio.Nombre} - {municipio.CodigoPostal}
+                    </option>
+                  )
+                }) : <option value={-1}>No hay municipios registrados</option>
+              }
+            </select>
+            <i className='fa-solid fa-caret-down'></i>
+          </div>
+        </div>
+      </main>
+      <footer>
+        <div className={`formAnswerContainer${answerActive ? ' active': ''}`}>
+          <div className={`formAnswer ${answerType}`}>{answer}</div>
+        </div>
+        <button type='submit' className='button primary'>Editar dirección</button>
       </footer>
     </form>
   )
