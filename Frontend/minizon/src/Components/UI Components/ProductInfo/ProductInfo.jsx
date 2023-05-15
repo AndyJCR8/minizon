0b9d82ae62761e0e7d2bbd6718922831dc001a0a
Andy Cáceres <andy_cr@umes.edu.gt>
2023-05-15 16:58:42 -0600

    Avances en los detalles del producto

--- Frontend/minizon/src/Components/UI Components/ProductInfo/ProductInfo.jsx
+++ Frontend/minizon/src/Components/UI Components/ProductInfo/ProductInfo.jsx
@@ -1,15 +1,29 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import './ProductInfo.scss'
 import React, { Children, cloneElement } from 'react'
 
 export default function ProductInfo({children}) {
 
   const [showInfo, setShowInfo] = useState(false);
+  const [active, setActive] = useState(false);
   const [productData, setProductData] = useState({});
 
+  const [productCount, setProductCount] = useState(0);
+  const [counter, setCounter] = useState(1);
+
   useEffect(() => {
-    
+    setProductCount(productData.Cantidad)
   }, [productData]);
+
+  const handleProductCount = (plus) => {
+    if(counter < productCount && plus) { setCounter(last => last + 1) }
+    if(counter > 1 && !plus) { setCounter(last => last - 1) }
+  }
+
+  useEffect(() => {
+    setTimeout(() => { setActive(showInfo) }, 100);
+    setCounter(1)
+  }, [showInfo]);
 
   return (
     <>
@@ -24,8 +38,9 @@ export default function ProductInfo({children}) {
       }
       {
         showInfo && Object.keys(productData ?? {}).length > 0 ?
-        <div className={`productSpecificInfoContainer${showInfo ? ' active' : ''}`}>
+        <div className={`productSpecificInfoContainer${active ? ' active' : ''}`}>
           <div className='productSpecificInfo'>
+            <div className='closeContainer'> <button onClick={() => { setActive(false); setTimeout(() => { setShowInfo(false) }, 250); }} className='button secondary'><i className='fa-solid fa-xmark'></i></button> </div>
             <header>
               <img src={`${productData.Imagen}`}/>
             </header>
@@ -33,7 +48,19 @@ export default function ProductInfo({children}) {
               <div className='productName'><p>{productData.Nombre}</p></div>
               <div className='productDescription'><p>{productData.Descripcion}</p></div>
               <div className='productDetails'>
-                <p>{productData.Descripcion}</p>
+                <p className='productPrice'>Q{productData.PrecioDeVenta}</p>
+                <div className='productCount'>
+                  <div className='minusCounter'>
+                    <button onClick={() => { handleProductCount(false) }} className='button primary minusButton'><i className='fa-solid fa-minus'></i></button>
+                  </div>
+                  <p className='count'>{counter}</p>
+                  <div className='plusCounter'>
+                    <button onClick={() => { handleProductCount(true) }} className='button primary plusButton'><i className='fa-solid fa-plus'></i></button>
+                  </div>
+                </div>
+                <div className='addToCartContainer'>
+                  <button className='button secondary'><i className='fa-solid fa-shopping-cart'></i></button>
+                </div>
               </div>
             </main>
           </div>
