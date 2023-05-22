0814ff04fd02ac062b9fed6d4bbd5034d1556b6d
AndyJCR8 <andy_cr@umes.edu.gt>
2023-05-21 21:29:57 -0600

    Cambios menores en el carrito de compras

--- Frontend/minizon/src/Pages/ShoppingCart/ShoppingCart.jsx
+++ Frontend/minizon/src/Pages/ShoppingCart/ShoppingCart.jsx
@@ -1,8 +1,55 @@
+import Loader from '../../Components/UI Components/Loader/Loader';
+import { getCart } from '../../Services/CartService';
 import './ShoppingCart.scss'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function ShoppingCart() {
+
+  const [loading, setLoading] = useState(true);
+  const [productsData, setProductsData] = useState([]);
+  const [subTotal, setSubTotal] = useState(0);
+
+  useEffect(() => {
+    const cart = getCart()
+    if(cart.length > 0)
+      setSubTotal(cart.reduce((acc, val) => acc + (parseFloat(val.PrecioDeVenta) * parseInt(val.Cantidad)), 0))
+
+    setProductsData(cart)
+    setLoading(false)
+  }, []);
+
   return (
-    <div>ShoppingCart</div>
+    <>
+      {
+        loading && <Loader size="xxl" text="cargando"/>
+      }
+      {
+        !loading &&
+        <div className='cartContainer'>
+          <div className='productsDetails'>
+            {
+              productsData.length > 0 &&
+              productsData.map(product => {
+                return (
+                  <>
+                    {JSON.stringify(product)}
+                  </>
+                )
+              })
+            }
+          </div>
+          <div className='extras'>
+            <header>
+              <p>Subtotal</p>
+              <p>Q{subTotal}</p>
+            </header>
+            <main>
+              <button className='button'><i className='fa-solid fa-lock'></i>Iniciar compra</button>
+              <button className='button'>Buscar más productos</button>
+            </main>
+          </div>
+        </div>
+      }
+    </>
   )
 }
